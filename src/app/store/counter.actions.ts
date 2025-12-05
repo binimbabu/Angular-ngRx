6330485aa4ee51b39472201f6dac8527b2fae045
Bini Babu <binibabu22@gmail.com>
2025-12-05 10:24:17 +0530

    ngrx

---
+++ src/app/store/counter.actions.ts
@@ -0,0 +1,16 @@
+import { Action, createAction, props } from '@ngrx/store';
+
+// export const increment = createAction(
+//   '[Counter] Increment',
+//   props<{ value: number }>()
+// );
+
+// export const decrement = createAction(
+//   '[Counter] Decrement',
+//   props<{ value: number }>()
+// );
+
+export class IncrementAction implements Action {
+  readonly type = '[Counter] Increment';
+  constructor(public value: number) {}
+}
